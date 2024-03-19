0dd12ce4ad6cb6b87560bab0f4540efe2216511a
GyancarlosPinto <gyancarlospinto@pursuit.org>
2024-03-18 21:42:10 -0400

    Intialing TaskCard Component

---
+++ frontend/src/components/TaskCard/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useNavigate } from 'react-router-dom';
+import { deleteTask } from '../../helpers/dataFetching';
+
+export default function TaskCard({ id, title, description, status, onTaskDelete }) {
+    let navigate = useNavigate();
+
+    const handleEdit = () => {
+        navigate(`/tasks/${id}`)
+    }
+
+    const handleDelete = async () => {
+        const success = await deleteTask(id);
+        if (success) {
+            onTaskDelete(id);
+        }
+    }
+
+    return (
+        <div className="task-card">
+            <h3>{title}</h3>
+            <p>{description}</p>
+            <p>{status}</p>
+            <div className="task-card-footer">
+                <button onClick={handleEdit} className="edit-button">Edit</button>
+                <button onClick={handleDelete} className="delete-button">Delete</button>
+            </div>
+        </div>
+
+    )
+}
